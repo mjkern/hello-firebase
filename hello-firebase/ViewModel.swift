ce05cc0bef2fffcad80165a2b31367ad44a36a95
Matt Kern <mjkern14@gmail.com>
2020-11-10 12:14:07 -0500

    demonstrates async loading after tap

--- hello-firebase/ViewModel.swift
+++ hello-firebase/ViewModel.swift
@@ -13,10 +13,14 @@ class ViewModel: ObservableObject {
   @Published var message = "hard coded message"
   var ref = Database.database().reference()
   func fetchData() {
-    ref.child("hello").observeSingleEvent(of: .value, with: { (snapshot) in
-      self.message = snapshot.value as! String
-    }) { (error) in
-      print(error.localizedDescription)
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+      // https://stackoverflow.com/questions/38031137/how-to-program-a-delay-in-swift-3
+      self.ref.child("hello").observeSingleEvent(of: .value, with: { (snapshot) in
+        self.message = snapshot.value as! String
+      }) { (error) in
+        print(error.localizedDescription)
+      }
     }
+    
   }
 }
