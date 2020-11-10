2135d49f0154a058eb75ae855d2e6ccc7380b50d
Matt Kern <mjkern14@gmail.com>
2020-11-10 10:36:30 -0500

    Initial Commit

---
+++ hello-firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-firebase
+//
+//  Created by Matt Kern on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
